a5259706e2dcb1632295fd4a05685ff5a5c27393
benfenyu <yujingbo_bf@163.com>
2020-08-05 19:56:17 +0800

    download model

--- run_server.sh
+++ run_server.sh
@@ -1,1 +1,1 @@
-python train.py --gpu_ids   5   --no_dropout     --no_flip   --preprocess    resize   --save_epoch_freq   10   --n_epochs_decay   50   --n_epochs   50   --batch_size   8   --num_threads   4
+python train.py --gpu_ids   4   --no_dropout     --no_flip   --preprocess    resize   --save_epoch_freq   10   --n_epochs_decay   50   --n_epochs   50   --batch_size   1   --num_threads   4
